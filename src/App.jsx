03db7e69737b939cb6abf147be0de73c96347743
Max <githubmaavel@gmail.com>
2024-11-08 18:41:16 +0600

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import AdminPanel from "./components/AdminPanel/AdminPanel";
 import PublicSession from "./components/PublisSession/PublicSession";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true }}>
         <Routes>
           <Route path="/" element={<PublicSession />} />
           <Route path="/adminPanel" element={<AdminPanel />} />
